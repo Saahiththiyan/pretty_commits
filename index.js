785d8e30bbaed6265dad690585d5fff65ef68da5
Saahiththiyan Mathivathanan <saahimathi@gmail.com>
2019-10-24 21:24:21 +0530

    feat: execute git command

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+var execProcess = require("./exec_process.js");
+execProcess.result("sh temp.sh", function(err, response) {
+  if (!err) {
+    console.log(response);
+  } else {
+    console.log(err);
+  }
+});
