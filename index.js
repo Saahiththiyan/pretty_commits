65873484c5834f781ea42f890610e3ac331b6c6b
Saahiththiyan Mathivathanan <saahimathi@gmail.com>
2019-10-25 18:42:58 +0530

    fix: print better online commit messages

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const child = require("child_process");
 const fs = require("fs");
 
-const output = child.execSync(`git log`).toString("utf-8");
+const output = child.execSync(`git log --oneline`).toString("utf-8");
 
 fs.writeFileSync("./temp/test.md", output, function(err) {
   if (err) {
