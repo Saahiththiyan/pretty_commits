0cb20c41290a298209aaf30947a55fd93d13373c
Saahiththiyan Mathivathanan <saahimathi@gmail.com>
2019-10-29 16:22:11 +0530

    feat: figlet added

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-const fs        = require("fs");
+const fs = require("fs");
+var figlet = require("figlet");
 const changeLog = require("./public/changeLogGenerator.js");
 
-const directory = './pretty';
+const directory = "./pretty";
 
 /**
  * Validate whether Directory exist
  * If does not exist create one.
  */
 const validateFileSystem = () => {
-	if (!fs.existsSync(directory)) {
-		fs.mkdirSync(directory);
-		console.log('\t Pretty folder created.');
-	}
-}
+  if (!fs.existsSync(directory)) {
+    fs.mkdirSync(directory);
+    console.log("\t Pretty folder created.");
+  }
+};
 
 console.log("--------------------------------------------");
 console.log("\t Pretty commit executing...");
@@ -23,3 +24,11 @@ changeLog.generateChangeLog(directory);
 console.log("\t Execution end.");
 console.log("--------------------------------------------");
 
+figlet("Pretty Commits", function(err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
